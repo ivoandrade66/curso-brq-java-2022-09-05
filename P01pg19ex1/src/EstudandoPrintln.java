0681471ccb4cfceafc6afb302030384fcdb7d3e2
Ivo Gomes de Andrade <andradeivo@brq.com>
2022-09-10 11:34:12 -0300

    adicionando pastas e pgms

---
+++ P01pg19ex1/src/EstudandoPrintln.java
@@ -0,0 +1,9 @@
+public class EstudandoPrintln {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("3 + 4");
+		System.out.println(3 + 4);
+		System.out.println(3 + "4");
+	}
+
+}
---
+++ P01pg19ex3/src/ImprimeRosto.java
@@ -0,0 +1,14 @@
+
+public class ImprimeRosto {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("  /////  ");
+		System.out.println(" | o o | ");
+		System.out.println("(|  ^  |)");
+		System.out.println(" | [_] | ");
+		System.out.println("  -----  ");
+
+	}
+
+}
