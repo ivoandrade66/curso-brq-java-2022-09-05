4f1a6665fc7f443b28e052d8350e60db148d0bd3
ivoandrade66 <112989760+ivoandrade66@users.noreply.github.com>
2022-09-29 19:26:30 -0300

    Clean Repo

---
+++ BoaNoite.java
@@ -0,0 +1,13 @@
+public class BoaNoite {
+
+	/* Este é meu primeiro pgm Java
+	 * ele exibe Boa Noite
+	 */
+	 
+	public static void main(String []args){
+		// exibe Boa Noite
+		System.out.println("Boa Noite");
+	}
+
+
+}
