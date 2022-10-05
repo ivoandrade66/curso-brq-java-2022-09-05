8e891a24c98514db0ae8f18956fcfbcfcab35f62
ivoandrade66 <112989760+ivoandrade66@users.noreply.github.com>
2022-10-05 19:23:17 -0300

    ajuste

--- ms01/src/main/java/com/brq/ms01/controllers/UsuarioController.java
+++ ms01/src/main/java/com/brq/ms01/controllers/UsuarioController.java
@@ -3,15 +3,16 @@ package com.brq.ms01.controllers;
 import com.brq.ms01.models.UsuarioModel;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 
 
-// comentário
+// commenter
 
 /*
- * comentário
+ * commenter
  * */
 
 @RestController
@@ -26,19 +27,24 @@ public class UsuarioController {
     @GetMapping("usuarios")
     public ArrayList<UsuarioModel> getAllUsuarios(){
 
-        UsuarioModel u = new UsuarioModel();
-        u.setId(1);
-        u.setNome("Manoel");
-        u.setEmail("manoel@gmail.com");
-
-        usuarios.add(u);
+//        UsuarioModel u = new UsuarioModel();
+//        u.setId(1);
+//        u.setNome("Manoel");
+//        u.setEmail("manoel@gmail.com");
+//
+//        usuarios.add(u);
 
         return usuarios;
     }
 
     @PostMapping("usuarios")
-    public String create(){
-        return "POST Usuários";
+    public UsuarioModel create(@RequestBody UsuarioModel usuario){
+    //public String create(@RequestBody UsuarioModel usuario){
+        System.out.println(usuario);
+        usuarios.add(usuario);
+        //return "POST Usuários";
+        return usuario;
     }
+
 
 }
