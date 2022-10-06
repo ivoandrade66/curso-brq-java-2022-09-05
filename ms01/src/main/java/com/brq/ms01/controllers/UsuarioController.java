a6aa41b4ff1ede77b7f49f9f79b9b2dc195a31b6
ivoandrade66 <112989760+ivoandrade66@users.noreply.github.com>
2022-10-05 21:18:28 -0300

    update 1

--- ms01/src/main/java/com/brq/ms01/controllers/UsuarioController.java
+++ ms01/src/main/java/com/brq/ms01/controllers/UsuarioController.java
@@ -1,10 +1,7 @@
 package com.brq.ms01.controllers;
 
 import com.brq.ms01.models.UsuarioModel;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 
@@ -19,6 +16,7 @@ import java.util.ArrayList;
 public class UsuarioController {
 
     // ESTE ARRAYLIST É DIDÁTICO, POIS ESTÁ SIMULANDO UM BANCO DE DADOS
+    private int counter = 1;
     private ArrayList<UsuarioModel> usuarios = new ArrayList<>();
 
     /*
@@ -40,11 +38,33 @@ public class UsuarioController {
     @PostMapping("usuarios")
     public UsuarioModel create(@RequestBody UsuarioModel usuario){
     //public String create(@RequestBody UsuarioModel usuario){
-        System.out.println(usuario);
+        //System.out.println(usuario);
+        usuario.setId( counter );
         usuarios.add(usuario);
+        counter++;
+        System.out.println(usuario);
+
+        //usuarios.add(usuario);
         //return "POST Usuários";
         return usuario;
     }
 
+    // /usuarios/1 --> o valor do id vai ser 1
+    @PatchMapping("usuarios/{id}")
+    public UsuarioModel update(@RequestBody UsuarioModel usuarioBody,
+                               @PathVariable int id ){
+        // como achar o usuário a ser alterado?
+        for ( int i = 0; i <  usuarios.size(); i++ ){
+            if (usuarios.get(i).getId() == id){
+                // achamos o usuário a ser alterado
+                usuarios.get(i).setNome( usuarioBody.getNome() );
+                usuarios.get(i).setEmail( usuarioBody.getEmail() );
 
+                return usuarios.get(i);
+            }//if
+
+        }// for
+
+        return null;
+    }// update()
 }
